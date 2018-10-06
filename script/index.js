e091d43bff39e96f8c26961a79f5c47624f1357c
Evan Hall <evan.tom.hall@gmail.com>
2018-10-06 02:12:54 -0400

    Implemented options menu

--- script/index.js
+++ script/index.js
@@ -1,13 +1,5 @@
 var editor = null;
 var parser = null;
-const options = {
-    comments: true,
-    extendedIdentifiers: false,
-    locations: false,
-    luaVersion: "5.1",
-    ranges: false,
-    scope: false
-};
 const optionDisabledColor = "#272822";
 const optionEnabledColor = "#97b757";
 const optionHoverColor = "#f92672";
@@ -23,13 +15,7 @@ function onload() {
     });
     editor.on("cursorActivity", onCursorActivity);
 
-    // Install event handlers.
-    document.getElementsByClassName("Option").forEach((button) => {
-        button.addEventListener("click", toggleOption);
-        button.addEventListener("mouseenter", doHighlight);
-        button.addEventListener("mouseleave", doHighlightReset);
-    });
-
+    // Redraw Options Menu backgrounds
     setOptionMenuColor();
 }
 
@@ -38,11 +24,7 @@ function onload() {
 function doLuaParseOrReset() {
     // If we don't have a lex session.
     if (parser === null) {
-        console.log(luaparse.parse(getTextAreaContents(), {
-            locations: true,
-            scope: true,
-            comments: false
-        }));
+        console.log(luaparse.parse(getTextAreaContents(), getOptionsAsObject()));
     }
     else {
         document.getElementById("lex").innerText = "Lex";
@@ -51,18 +33,18 @@ function doLuaParseOrReset() {
     }
 }
 
-function doHighlight(event) {
-    event.relatedTarget.style.backgroundColor = optionHoverColor;
+function doHighlight(button) {
+    button.style.backgroundColor = optionHoverColor;
 }
 
-function doHighlightReset(event) {
-    event.relatedTarget.style.backgroundColor = (event.relatedTarget.value.localeCompare("true") === 0) ? optionEnabledColor : optionDisabledColor;
+function doHighlightReset(button) {
+    button.style.backgroundColor = (Boolean(button.value)) ? optionEnabledColor : optionDisabledColor;
 }
 
 function doLuaLex() {
     // If we don't have a lex session.
     if (parser === null) {
-        parser = luaparse.parse(getTextAreaContents(), { wait: true });
+        parser = luaparse.parse(getTextAreaContents(), getOptionsAsObject(wait=true));
 
         document.getElementById("lex").innerText = "Next";
         document.getElementById("parse").innerText = "Reset";
@@ -77,42 +59,43 @@ function onCursorActivity(instance) {
     document.getElementById("offset").innerText = offset;
 }
 
-function toggleOption(event) {
-    switch (event.relatedTarget.id) {
+function toggleOption(button) {
+    switch (button.id) {
         case "opt-comments":
-            event.relatedTarget.value = "true";
-            break;
         case "opt-x-ids":
-            options.extendedIdentifiers = ! options.extendedIdentifiers;
-            break;
         case "opt-locations":
-            options.locations = ! options.locations;
-            break;
-        case "opt-v51":
-        case "opt-v52":
-        case "opt-v53":
-            // The Lua Version option should only have 1 submenu.
-            if (suboption !== undefined && suboption.length === 1) {
-                switch (suboption[0]) {
-                    case 1:
-                        options.luaVersion = "5.1";
-                        break;
-                    case 2:
-                        options.luaVersion = "5.2";
-                        break;
-                    case 3:
-                        options.luaVersion = "5.3";
-                        break;
-                    default:
-                        break;
-                }
+        case "opt-ranges":
+        case "opt-scope":
+            if (Boolean(button.value)) {
+                button.value = "";
+            }
+            else {
+                button.value = "true";
             }
             break;
-        case "opt-ranges":
-            options.ranges = ! options.ranges;
+        case "opt-v51":
+            // Only acknowledge click if button value is false (blank).
+            if (!Boolean(button.value)) {
+                button.value = "true";
+                document.getElementById("opt-v52").value = "";
+                document.getElementById("opt-v53").value = "";
+            }
             break;
-        case "opt-scope":
-            options.scope = ! options.scope;
+        case "opt-v52":
+            // Only acknowledge click if button value is false (blank).
+            if (!Boolean(button.value)) {
+                document.getElementById("opt-v51").value = "";
+                button.value = "true";
+                document.getElementById("opt-v53").value = "";
+            }
+            break;
+        case "opt-v53":
+            // Only acknowledge click if button value is false (blank).
+            if (!Boolean(button.value)) {
+                document.getElementById("opt-v51").value = "";
+                document.getElementById("opt-v52").value = "";
+                button.value = "true";
+            }
             break;
         default:
             break;
@@ -123,6 +106,38 @@ function toggleOption(event) {
 
 /* Utilities */
 
+function getOptionsAsObject(wait=false) {
+    var options = { wait };
+
+    // Comments
+    options.comments = Boolean(document.getElementById("opt-comments").value);
+
+    // Extended Identifiers
+    options.extendedIdentifiers = Boolean(document.getElementById("opt-x-ids").value);
+
+    // Locations
+    options.locations = Boolean(document.getElementById("opt-locations").value);
+
+    // Lua Version
+    if (Boolean(document.getElementById("opt-v52").value)) {
+        options.luaVersion = "5.2";
+    }
+    else if (Boolean(document.getElementById("opt-v53").value)) {
+        options.luaVersion = "5.3";
+    }
+    else {
+        options.luaVersion = "5.1";
+    }
+
+    // Ranges
+    options.ranges = Boolean(document.getElementById("opt-ranges").value);
+
+    // Scope
+    options.scope = Boolean(document.getElementById("opt-scope").value);
+
+    return options;
+}
+
 function getTextAreaContents() {
     var text = editor.getValue();
     // Add a newline to guarantee everything is parsed.
@@ -130,29 +145,6 @@ function getTextAreaContents() {
 }
 
 function setOptionMenuColor() {
-    const getLuaOptionStruct = (version) => {
-        const result = { v51: false, v52: false, v53: false };
-
-        switch (version) {
-            case "5.1":
-                result.v51 = true;
-                break;
-
-            case "5.2":
-                result.v52 = true;
-                break;
-
-            case "5.3":
-                result.v53 = true;
-                break;
-
-            default:
-                break;
-        }
-
-        return result;
-    };
-
     const getOptionColor = (optionBool) => {
         return (optionBool) ? optionEnabledColor : optionDisabledColor;
     };
@@ -161,33 +153,32 @@ function setOptionMenuColor() {
 
     // Comments
     button = document.getElementById("opt-comments");
-    button.style.backgroundColor = getOptionColor(options.comments);
+    button.style.backgroundColor = getOptionColor(Boolean(button.value));
 
     // Extended Identifiers
     button = document.getElementById("opt-x-ids");
-    button.style.backgroundColor = getOptionColor(options.extendedIdentifiers);
+    button.style.backgroundColor = getOptionColor(Boolean(button.value));
 
     // Locations
     button = document.getElementById("opt-locations");
-    button.style.backgroundColor = getOptionColor(options.locations);
+    button.style.backgroundColor = getOptionColor(Boolean(button.value));
 
     // Lua Version
-    const versionStruct = getLuaOptionStruct(options.luaVersion);
     // 5.1
     button = document.getElementById("opt-v51");
-    button.style.backgroundColor = getOptionColor(versionStruct.v51);
+    button.style.backgroundColor = getOptionColor(Boolean(button.value));
     // 5.2
     button = document.getElementById("opt-v52");
-    button.style.backgroundColor = getOptionColor(versionStruct.v52);
+    button.style.backgroundColor = getOptionColor(Boolean(button.value));
     // 5.3
     button = document.getElementById("opt-v53");
-    button.style.backgroundColor = getOptionColor(versionStruct.v53);
+    button.style.backgroundColor = getOptionColor(Boolean(button.value));
 
     // Ranges
     button = document.getElementById("opt-ranges");
-    button.style.backgroundColor = getOptionColor(options.ranges);
+    button.style.backgroundColor = getOptionColor(Boolean(button.value));
 
     // Scope
     button = document.getElementById("opt-scope");
-    button.style.backgroundColor = getOptionColor(options.scope);
+    button.style.backgroundColor = getOptionColor(Boolean(button.value));
 }
